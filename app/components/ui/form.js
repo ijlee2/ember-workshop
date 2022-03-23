3fd4dd7215e8b76551273a7c74be4288c2bf12e9
Isaac Lee <ijlee2@users.noreply.github.com>
2022-03-23 16:05:08 +0100

    Created the <Ui::Form::Information> component

--- app/components/ui/form.js
+++ app/components/ui/form.js
@@ -1,8 +1,11 @@
 import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class UiFormComponent extends Component {
+  formId = guidFor(this);
+
   @tracked changeset = this.args.data ?? {};
 
   @action submitForm(event) {
