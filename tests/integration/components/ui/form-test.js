eb599ee2d6c39d7e4afc4e70add5d9d66b330015
Isaac Lee <ijlee2@users.noreply.github.com>
2022-03-23 15:24:04 +0100

    Created the <Ui::Form> component

---
+++ tests/integration/components/ui/form-test.js
@@ -0,0 +1,77 @@
+import { click, render } from '@ember/test-helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
+import { setupRenderingTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+import sinon from 'sinon';
+
+module('Integration | Component | ui/form', function (hooks) {
+  setupRenderingTest(hooks);
+  setupIntl(hooks);
+
+  test('The component renders a form', async function (assert) {
+    this.submitForm = sinon.spy();
+
+    await render(hbs`
+      <Ui::Form
+        @data={{hash
+          donation=undefined
+          email=undefined
+          message="I 🧡 container queries!"
+          name=undefined
+          subscribe=true
+        }}
+        @instructions="Still have questions about ember-container-query? Try sending me a message."
+        @onSubmit={{this.submitForm}}
+        @title="Contact me"
+      />
+    `);
+
+    assert
+      .dom('[data-test-form="Contact me"]')
+      .exists({ count: 1 }, 'We see the form.');
+
+    assert.dom('[data-test-field]').exists({ count: 0 }, 'We see 0 fields.');
+
+    assert
+      .dom('[data-test-button="Submit"]')
+      .hasAttribute('type', 'submit', 'We see the correct type.')
+      .hasTagName('button', 'We see the correct tag name.')
+      .hasText('Submit', 'We see the submit button.');
+
+    await a11yAudit();
+
+    assert.ok(true, 'We passed the accessibility audit.');
+  });
+
+  test('We can submit the form', async function (assert) {
+    this.submitForm = sinon.spy();
+
+    await render(hbs`
+      <Ui::Form
+        @data={{hash
+          donation=undefined
+          email=undefined
+          message="I 🧡 container queries!"
+          name=undefined
+          subscribe=true
+        }}
+        @onSubmit={{this.submitForm}}
+      />
+    `);
+
+    await click('[data-test-button="Submit"]');
+
+    assert.true(
+      this.submitForm.calledOnceWith({
+        donation: undefined,
+        email: undefined,
+        message: 'I 🧡 container queries!',
+        name: undefined,
+        subscribe: true,
+      }),
+      'We called @onSubmit once.',
+    );
+  });
+});
