7e4c4feb323076c0c304493fe221362f4edb2c26
Isaac Lee <ijlee2@users.noreply.github.com>
2022-03-24 09:18:11 +0100

    Day 1. Extended <Ui::Form::Input> to handle the number type

--- app/components/ui/form/input.js
+++ app/components/ui/form/input.js
@@ -27,8 +27,20 @@ export default class UiFormInputComponent extends Component {
   }
 
   @action updateValue(event) {
-    const { key, onUpdate } = this.args;
+    const { key, onUpdate, type } = this.args;
     const { value } = event.target;
+
+    if (type === 'number') {
+      const valueAsNumber = Number.parseFloat(value);
+
+      if (Number.isNaN(valueAsNumber)) {
+        onUpdate({ key, value: undefined });
+        return;
+      }
+
+      onUpdate({ key, value: valueAsNumber });
+      return;
+    }
 
     onUpdate({ key, value });
   }
