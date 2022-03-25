bfd1579a5c2a2ba2f7ad816c59b43019597dbd17
Isaac Lee <ijlee2@users.noreply.github.com>
2022-03-25 14:24:49 +0100

    Day 2. Fetched all products

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ProductsRoute extends Route {}
+export default class ProductsRoute extends Route {
+  @service store;
+
+  model() {
+    return this.store.findAll('product');
+  }
+}
