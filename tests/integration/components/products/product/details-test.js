c0d53b5392e669a0c2968955f4bd01e102a0e62f
ijlee2 <ijlee2@users.noreply.github.com>
2022-04-11 17:40:23 +0200

    Day 3. Added rendering tests for the <Products::Product::Details> component

--- tests/integration/components/products/product/details-test.js
+++ tests/integration/components/products/product/details-test.js
@@ -1,9 +1,10 @@
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { a11yAudit } from 'ember-a11y-testing/test-support';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupIntl } from 'ember-intl/test-support';
 import { setupRenderingTest } from 'ember-qunit';
 import { module, test } from 'qunit';
+import sinon from 'sinon';
 
 module('Integration | Component | products/product/details', function (hooks) {
   setupRenderingTest(hooks);
@@ -36,9 +37,31 @@ module('Integration | Component | products/product/details', function (hooks) {
       />
     `);
 
-    /*
-      TODO: Write tests
-    */
+    assert
+      .dom('[data-test-field="Name"]')
+      .hasText('Vanilla Ice Cream Cake', 'We see the product name.');
+
+    assert
+      .dom('[data-test-field="Description"]')
+      .hasText('Made with organic herbs', 'We see the product description.');
+
+    assert
+      .dom('[data-test-field="Price"]')
+      .hasText('$40', 'We see the product price.');
+
+    assert
+      .dom('[data-test-field="Rating"]')
+      .hasText('4.5 out of 5 stars', 'We see the product rating.');
+
+    assert
+      .dom('[data-test-field="Seller"]')
+      .hasText("Amy's", 'We see the product seller.');
+
+    assert
+      .dom('[data-test-button="Add to Cart"]')
+      .hasAttribute('type', 'button', 'We see the correct type.')
+      .hasTagName('button', 'We see the correct tag name.')
+      .hasText('Add to Cart', 'We see the add to cart button.');
 
     await a11yAudit();
 
@@ -46,16 +69,21 @@ module('Integration | Component | products/product/details', function (hooks) {
   });
 
   test('We can click on the add to cart button', async function (assert) {
+    const stubbedLog = sinon.stub(console, 'log');
+
     await render(hbs`
       <Products::Product::Details
         @product={{this.product}}
       />
     `);
 
-    /*
-      TODO: Write tests
-    */
+    await click('[data-test-button="Add to Cart"]');
 
-    assert.ok(true);
+    assert.true(
+      stubbedLog.calledOnceWith(
+        'Vanilla Ice Cream Cake has been added to the cart.',
+      ),
+      'We logged a message to the user.',
+    );
   });
 });
