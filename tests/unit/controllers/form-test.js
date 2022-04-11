fa3bd3c22e4779a01fe23496350d73ceb02869f0
ijlee2 <ijlee2@users.noreply.github.com>
2022-04-11 16:27:31 +0200

    Added unit tests for the form controller

--- tests/unit/controllers/form-test.js
+++ tests/unit/controllers/form-test.js
@@ -1,12 +1,82 @@
+import { setupMirage } from 'ember-cli-mirage/test-support';
 import { setupTest } from 'ember-qunit';
+import { assignVariants } from 'ember-workshop/tests/helpers';
 import { module, test } from 'qunit';
 
 module('Unit | Controller | form', function (hooks) {
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it exists', function (assert) {
-    let controller = this.owner.lookup('controller:form');
-    assert.ok(controller);
+  hooks.beforeEach(function () {
+    this.controller = this.owner.lookup('controller:form');
+  });
+
+  module('initialData', function () {
+    test('subscribe-to-ember-times, control', function (assert) {
+      assignVariants({
+        'subscribe-to-ember-times': 'control',
+      });
+
+      assert.deepEqual(
+        this.controller.initialData,
+        {
+          donation: undefined,
+          email: undefined,
+          message: 'I 🧡 container queries!',
+          name: undefined,
+        },
+        'We get the correct value.'
+      );
+    });
+
+    test('subscribe-to-ember-times, v1', function (assert) {
+      assignVariants({
+        'subscribe-to-ember-times': 'v1',
+      });
+
+      assert.deepEqual(
+        this.controller.initialData,
+        {
+          email: undefined,
+          message: 'I 🧡 container queries!',
+          name: undefined,
+          subscribe: true,
+        },
+        'We get the correct value.'
+      );
+    });
+  });
+
+  module('submitForm', function (nestedHooks) {
+    setupMirage(nestedHooks);
+
+    test('Calls POST /contact-me', async function (assert) {
+      assert.expect(1);
+
+      this.server.post('/contact-me', (schema, request) => {
+        const json = JSON.parse(request.requestBody);
+
+        assert.deepEqual(
+          json,
+          {
+            donation: 0,
+            email: '',
+            message: 'I 🧡 container queries!',
+            name: null,
+            subscribe: false,
+          },
+          'We sent the correct request body.'
+        );
+      });
+
+      const data = {
+        donation: 0,
+        email: '',
+        message: 'I 🧡 container queries!',
+        name: undefined,
+        subscribe: false,
+      };
+
+      await this.controller.submitForm.perform(data);
+    });
   });
 });
