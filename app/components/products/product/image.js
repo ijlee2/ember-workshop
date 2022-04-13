d23b7f060abce4b8f9cc63197329d9fcb2f15983
ijlee2 <ijlee2@users.noreply.github.com>
2022-04-13 09:22:26 +0200

    Created the <Products::Product::Image> component

---
+++ app/components/products/product/image.js
@@ -0,0 +1,6 @@
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+
+export default class ProductsProductImageComponent extends Component {
+  @service config;
+}
