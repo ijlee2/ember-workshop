3beb97a2cd0a529b0578b89ed0b793d525a5624c
ijlee2 <ijlee2@users.noreply.github.com>
2022-04-19 10:25:33 +0200

    New in Ember v4.4: Used the {{unique-id}} helper to generate IDs

--- app/components/ui/form.js
+++ app/components/ui/form.js
@@ -1,11 +1,8 @@
 import { action } from '@ember/object';
-import { guidFor } from '@ember/object/internals';
 import Component from '@glimmer/component';
 import { TrackedObject } from 'tracked-built-ins';
 
 export default class UiFormComponent extends Component {
-  formId = guidFor(this);
-
   changeset = new TrackedObject(this.args.data ?? {});
 
   @action submitForm(event) {
