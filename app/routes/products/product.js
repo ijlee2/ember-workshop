63bdf5c5d38b08926102d3009dacbdd5cdc1227e
ijlee2 <ijlee2@users.noreply.github.com>
2022-10-10 17:01:43 +0200

    Day 2. Fetched a particular product

--- app/routes/products/product.js
+++ app/routes/products/product.js
@@ -1,3 +1,18 @@
+import { action } from '@ember/object';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ProductsProductRoute extends Route {}
+export default class ProductsProductRoute extends Route {
+  @service router;
+  @service store;
+
+  model(params) {
+    const { id } = params;
+
+    return this.store.findRecord('product', id);
+  }
+
+  @action error(/* error, transition */) {
+    this.router.replaceWith('products');
+  }
+}
