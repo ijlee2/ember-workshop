af29145b93789031d2cce3c09b91e68adcd4532c
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:29:25 +0200

    Created the <Ui::Page> component

--- my-addon/src/template-registry.ts
+++ my-addon/src/template-registry.ts
@@ -1,5 +1,7 @@
 import type NavigationMenuComponent from './components/navigation-menu.gts';
+import type UiPageComponent from './components/ui/page.gts';
 
 export default interface MyAddonRegistry {
   NavigationMenu: typeof NavigationMenuComponent;
+  'Ui::Page': typeof UiPageComponent;
 }
