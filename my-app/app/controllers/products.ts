37a0310f1f75e506b3813cef1a220cd35e54fcef
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:33:02 +0200

    Day 2. Rendered the filter-by-name input

--- my-app/app/controllers/products.ts
+++ my-app/app/controllers/products.ts
@@ -1,4 +1,6 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 import type { Model } from '../routes/products';
 import styles from './products.css';
@@ -7,4 +9,25 @@ export default class ProductsController extends Controller {
   declare model: Model;
 
   styles = styles;
+
+  @tracked name: string | null = null;
+
+  @action updateQueryParameters({
+    key,
+    value,
+  }: {
+    key: string;
+    value: any;
+  }): void {
+    if (key !== 'name') {
+      return;
+    }
+
+    if (value === undefined || value === '') {
+      this[key] = null;
+      return;
+    }
+
+    this[key] = value;
+  }
 }
