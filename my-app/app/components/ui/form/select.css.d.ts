4fc117df3c0c5acb5ce31aa687179f3c13f01209
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:31:54 +0200

    Day 1 Homework: Created the <Ui::Form::Select> component

---
+++ my-app/app/components/ui/form/select.css.d.ts
@@ -0,0 +1,8 @@
+declare const styles: {
+  readonly 'clear-button': string;
+  readonly 'is-disabled': string;
+  readonly 'select': string;
+  readonly 'select-container': string;
+};
+
+export default styles;
