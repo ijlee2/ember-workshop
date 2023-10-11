0b6fa124a1133e22367505a3c087c0b35a66f52d
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:33:07 +0200

    Day 2. Queried products

--- my-app/app/routes/products.ts
+++ my-app/app/routes/products.ts
@@ -4,10 +4,26 @@ import { type Registry as Services, service } from '@ember/service';
 import type { ModelFrom } from '../utils/routes';
 import type { Product } from '../utils/routes/products';
 
+type Params = {
+  name: string | null;
+};
+
 export default class ProductsRoute extends Route {
   @service declare api: Services['api'];
 
-  model(): Promise<Product[]> {
+  queryParams = {
+    name: {
+      refreshModel: true,
+    },
+  };
+
+  model(params: Params): Promise<Product[]> {
+    const { name } = params;
+
+    if (name) {
+      return this.api.get<Product[]>(`/products?name=${name}`);
+    }
+
     return this.api.get<Product[]>('/products');
   }
 }
