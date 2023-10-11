da826f7b7771246b43e6de7a3da163425189db81
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:27:24 +0200

    Created a workspace

---
+++ configs/eslint/ember/app/index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'ember',
+    '@typescript-eslint',
+    'simple-import-sort',
+    'typescript-sort-keys',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'plugin:typescript-sort-keys/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {
+    curly: 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.gjs', '.gts', '.js', '.ts'],
+      },
+      typescript: true,
+    },
+  },
+  overrides: [
+    // Ember files
+    {
+      files: ['**/*.{gts,ts}'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      rules: {
+        '@typescript-eslint/array-type': 'error',
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/no-empty-object-type': [
+          'error',
+          {
+            allowInterfaces: 'always',
+            allowObjectTypes: 'always',
+          },
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-import-type-side-effects': 'error',
+      },
+    },
+    {
+      files: ['**/*.gjs'],
+      parser: 'ember-eslint-parser',
+      extends: ['plugin:ember/recommended-gjs'],
+    },
+    {
+      files: ['**/*.gts'],
+      parser: 'ember-eslint-parser',
+      extends: ['plugin:ember/recommended-gts'],
+    },
+    {
+      files: ['**/*.{gjs,gts,js,ts}'],
+      rules: {
+        'import/no-duplicates': 'error',
+        'import/no-unresolved': [
+          'error',
+          { ignore: ['^@ember', '^dummy/', '^ember', 'fetch'] },
+        ],
+      },
+    },
+    // Node files
+    {
+      files: [
+        './.eslintrc.{cjs,js}',
+        './.prettierrc.{cjs,js}',
+        './.stylelintrc.{cjs,js}',
+        './.template-lintrc.{cjs,js}',
+        './ember-cli-build.js',
+        './postcss.config.js',
+        './testem.js',
+        './vite.config.{js,mjs}',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './lib/*/index.js',
+        './server/**/*.js',
+      ],
+      env: {
+        browser: false,
+        node: true,
+      },
+      extends: ['plugin:n/recommended'],
+    },
+    // Test files
+    {
+      files: ['tests/**/*-test.{gjs,gts,js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+    },
+  ],
+};
---
+++ configs/prettier/.prettierrc.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  plugins: ['prettier-plugin-ember-template-tag'],
+  overrides: [
+    {
+      files: '*.css.d.ts',
+      options: {
+        quoteProps: 'preserve',
+      },
+    },
+    {
+      files: '*.{cjs,cts,js,mjs,mts,ts}',
+      options: {
+        printWidth: 80,
+        singleQuote: true,
+      },
+    },
+    {
+      files: '*.{gjs,gts}',
+      options: {
+        printWidth: 80,
+        singleQuote: true,
+        templateSingleQuote: false,
+      },
+    },
+    {
+      files: '*.hbs',
+      options: {
+        printWidth: 64,
+        singleQuote: false,
+      },
+    },
+  ],
+};
