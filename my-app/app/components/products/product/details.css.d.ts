c219cb0a26c8297b7d08ccb491cbd7fe6d551d1e
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:30:29 +0200

    Created the <Products::Product::Details> component

---
+++ my-app/app/components/products/product/details.css.d.ts
@@ -0,0 +1,11 @@
+declare const styles: {
+  readonly 'actions': string;
+  readonly 'body': string;
+  readonly 'container': string;
+  readonly 'field': string;
+  readonly 'header': string;
+  readonly 'image-container': string;
+  readonly 'name': string;
+};
+
+export default styles;
