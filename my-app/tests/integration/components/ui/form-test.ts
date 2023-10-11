75432e1a0dc83e5a57f11176cda40a7d39703eac
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:29:56 +0200

    Created the <Ui::Form::Textarea> component

--- my-app/tests/integration/components/ui/form-test.ts
+++ my-app/tests/integration/components/ui/form-test.ts
@@ -61,6 +61,13 @@ module('Integration | Component | ui/form', function (hooks) {
             @type="email"
           />
         </div>
+
+        <div>
+          <F.Textarea
+            @key="message"
+            @label="Message"
+          />
+        </div>
       </Ui::Form>
     `);
 
@@ -78,7 +85,7 @@ module('Integration | Component | ui/form', function (hooks) {
       )
       .hasAria('labelledby', titleId, 'We see the correct aria-labelledby.');
 
-    assert.dom('[data-test-field]').exists({ count: 2 }, 'We see 2 fields.');
+    assert.dom('[data-test-field]').exists({ count: 3 }, 'We see 3 fields.');
 
     assert
       .dom('[data-test-button="Submit"]')
@@ -122,11 +129,19 @@ module('Integration | Component | ui/form', function (hooks) {
             @type="email"
           />
         </div>
+
+        <div>
+          <F.Textarea
+            @key="message"
+            @label="Message"
+          />
+        </div>
       </Ui::Form>
     `);
 
     await fillIn('[data-test-field="Name"]', 'Zoey');
     await fillIn('[data-test-field="Email"]', 'zoey@emberjs.com');
+    await fillIn('[data-test-field="Message"]', 'Gude!');
 
     await click('[data-test-button="Submit"]');
 
@@ -134,7 +149,7 @@ module('Integration | Component | ui/form', function (hooks) {
       this.submitForm.calledOnceWith({
         donation: undefined,
         email: 'zoey@emberjs.com',
-        message: 'I 🧡 container queries!',
+        message: 'Gude!',
         name: 'Zoey',
         subscribe: true,
       }),
