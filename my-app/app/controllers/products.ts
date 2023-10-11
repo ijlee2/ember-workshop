f04e2ee641d1b74bb2db07ae21c15fb7f2d9c7f3
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:33:17 +0200

    Day 2. Created a restartable task

--- my-app/app/controllers/products.ts
+++ my-app/app/controllers/products.ts
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
+import { isTesting, macroCondition } from '@embroider/macros';
 import { tracked } from '@glimmer/tracking';
+import { restartableTask, timeout } from 'ember-concurrency';
 
 import type { Model } from '../routes/products';
 import styles from './products.css';
+
+const TIMEOUT_IN_MILLISECONDS = macroCondition(isTesting()) ? 1 : 300;
 
 export default class ProductsController extends Controller {
   declare model: Model;
@@ -13,22 +16,20 @@ export default class ProductsController extends Controller {
 
   styles = styles;
 
-  @action updateQueryParameters({
-    key,
-    value,
-  }: {
-    key: string;
-    value: any;
-  }): void {
-    if (key !== 'name') {
-      return;
-    }
+  updateQueryParameters = restartableTask(
+    async ({ key, value }: { key: string; value: any }) => {
+      if (key !== 'name') {
+        return;
+      }
 
-    if (value === undefined || value === '') {
-      this[key] = null;
-      return;
-    }
+      await timeout(TIMEOUT_IN_MILLISECONDS);
 
-    this[key] = value;
-  }
+      if (value === undefined || value === '') {
+        this[key] = null;
+        return;
+      }
+
+      this[key] = value;
+    },
+  );
 }
