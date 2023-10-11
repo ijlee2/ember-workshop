b1678a05c6183f875c96b0f41d50e17eb1685769
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:27:31 +0200

    Mocked the backend API

--- my-app/mirage/config.js
+++ my-app/mirage/config.js
@@ -1,4 +1,4 @@
-import { createServer } from 'miragejs';
+import { createServer, Response } from 'miragejs';
 
 function routes() {
   // These comments are here to help you get started. Feel free to delete them.
@@ -10,6 +10,13 @@ function routes() {
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Uncomment the next line to make the loading spinner
+    visible for some time.
+  */
+  // this.timing = 1000;
+
   /*
     Shorthand cheatsheet:
 
@@ -21,6 +28,27 @@ function routes() {
 
     https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
   */
+  this.post('/contact-me', () => {
+    return new Response(200);
+  });
+
+  this.get('/products', (schema, request) => {
+    const { name } = request.queryParams;
+    const products = schema.products.all();
+
+    if (!name) {
+      return products;
+    }
+
+    return products.filter((product) => {
+      const productName = (product.name ?? '').toLowerCase();
+      const target = name.toLowerCase();
+
+      return productName.includes(target);
+    });
+  });
+
+  this.get('/products/:id');
 }
 
 export default function (config) {
