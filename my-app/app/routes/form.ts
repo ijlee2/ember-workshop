f79ac0e125ec74af273bdc51c0315240c81a059f
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:36:43 +0200

    Day 2. Injected the experiments service into the form route

--- my-app/app/routes/form.ts
+++ my-app/app/routes/form.ts
@@ -1,3 +1,13 @@
+import type Owner from '@ember/owner';
 import Route from '@ember/routing/route';
+import { type Registry as Services, service } from '@ember/service';
 
-export default class FormRoute extends Route {}
+export default class FormRoute extends Route {
+  @service declare experiments: Services['experiments'];
+
+  constructor(owner: Owner) {
+    super(owner);
+
+    this.experiments.decideVariant('subscribe-to-ember-times');
+  }
+}
