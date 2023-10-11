76c3afc0b0cbc7e870884b97d6f3c4336797d2fa
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:37:09 +0200

    Day 3. Added rendering tests for the <Products::Product::Details> component

--- my-app/tests/integration/components/products/product/details-test.ts
+++ my-app/tests/integration/components/products/product/details-test.ts
@@ -1,4 +1,5 @@
 import {
+  click,
   render,
   type TestContext as BaseTestContext,
 } from '@ember/test-helpers';
@@ -8,6 +9,7 @@ import { setupIntl } from 'ember-intl/test-support';
 import { setupRenderingTest } from 'my-app/tests/helpers';
 import type { Product } from 'my-app/utils/routes/products';
 import { module, test } from 'qunit';
+import { stub } from 'sinon';
 
 interface TestContext extends BaseTestContext {
   product: Product;
@@ -38,9 +40,31 @@ module('Integration | Component | products/product/details', function (hooks) {
       />
     `);
 
-    /*
-      TODO: Write tests
-    */
+    assert
+      .dom('[data-test-field="Name"]')
+      .hasText('Vanilla Ice Cream Cake', 'We see the product name.');
+
+    assert
+      .dom('[data-test-field="Description"]')
+      .hasText('Made with organic herbs', 'We see the product description.');
+
+    assert
+      .dom('[data-test-field="Price"]')
+      .hasText('$40', 'We see the product price.');
+
+    assert
+      .dom('[data-test-field="Rating"]')
+      .hasText('4.5 out of 5 stars', 'We see the product rating.');
+
+    assert
+      .dom('[data-test-field="Seller"]')
+      .hasText("Amy's", 'We see the product seller.');
+
+    assert
+      .dom('[data-test-button="Add to Cart"]')
+      .hasAttribute('type', 'button', 'We see the correct type.')
+      .hasTagName('button', 'We see the correct tag name.')
+      .hasText('Add to Cart', 'We see the add to cart button.');
 
     await a11yAudit();
 
@@ -48,16 +72,23 @@ module('Integration | Component | products/product/details', function (hooks) {
   });
 
   test('We can click on the add to cart button', async function (this: TestContext, assert) {
+    const stubbedLog = stub(console, 'log');
+
     await render<TestContext>(hbs`
       <Products::Product::Details
         @product={{this.product}}
       />
     `);
 
-    /*
-      TODO: Write tests
-    */
+    await click('[data-test-button="Add to Cart"]');
 
-    assert.ok(true);
+    assert.true(
+      stubbedLog.calledOnceWith(
+        'Vanilla Ice Cream Cake has been added to the cart.',
+      ),
+      'We logged a message to the user.',
+    );
+
+    stubbedLog.restore();
   });
 });
