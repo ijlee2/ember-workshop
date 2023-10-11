46a3373f73afe54f949f27604112d7afed893520
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:36:43 +0200

    Day 2. Injected the experiments service into the form route

--- my-app/app/controllers/form.ts
+++ my-app/app/controllers/form.ts
@@ -6,17 +6,30 @@ import styles from './form.css';
 
 export default class FormController extends Controller {
   @service declare api: Services['api'];
+  @service declare experiments: Services['experiments'];
 
   styles = styles;
 
   get initialData(): Record<string, any> {
+    if (this.isPartOfSubscribeToEmberTimesExperiment) {
+      return {
+        email: undefined,
+        message: 'I 🧡 container queries!',
+        name: undefined,
+        subscribe: true,
+      };
+    }
+
     return {
       donation: undefined,
       email: undefined,
       message: 'I 🧡 container queries!',
       name: undefined,
-      subscribe: true,
     };
+  }
+
+  get isPartOfSubscribeToEmberTimesExperiment(): boolean {
+    return this.experiments.getVariant('subscribe-to-ember-times') === 'v1';
   }
 
   submitForm = dropTask(async (data: Record<string, any>): Promise<void> => {
