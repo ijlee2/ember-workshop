b61ec4409b5c9a1fcad55d5842ca4b4596f576b4
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:32:17 +0200

    Day 2. Fetched a particular product

--- my-app/app/routes/products/product.ts
+++ my-app/app/routes/products/product.ts
@@ -1,7 +1,23 @@
+import { action } from '@ember/object';
 import Route from '@ember/routing/route';
+import { type Registry as Services, service } from '@ember/service';
 
 import type { ModelFrom } from '../../utils/routes';
+import type { Product } from '../../utils/routes/products';
 
-export default class ProductsProductRoute extends Route {}
+export default class ProductsProductRoute extends Route {
+  @service declare api: Services['api'];
+  @service declare router: Services['router'];
+
+  model(params: { id: string }): Promise<Product> {
+    const { id } = params;
+
+    return this.api.get<Product>(`/products/${id}`);
+  }
+
+  @action error(/* error, transition */) {
+    this.router.replaceWith('products');
+  }
+}
 
 export type Model = ModelFrom<ProductsProductRoute>;
