642d524327fb13fecfe29426523fb40190cc407e
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:30:59 +0200

    Day 1. Illustrated DDAU

--- my-app/app/controllers/form.ts
+++ my-app/app/controllers/form.ts
@@ -1,7 +1,21 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
 import styles from './form.css';
 
 export default class FormController extends Controller {
   styles = styles;
+
+  get initialData(): Record<string, any> {
+    return {
+      email: undefined,
+      message: 'I 🧡 container queries!',
+      name: undefined,
+      subscribe: true,
+    };
+  }
+
+  @action async submitForm(data: Record<string, any>): Promise<void> {
+    console.table(data);
+  }
 }
