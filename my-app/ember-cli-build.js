140f834c23ac974e0b091f5591b67ca5f1dfce21
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-11 13:27:24 +0200

    Created a workspace

---
+++ my-app/ember-cli-build.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const sideWatch = require('@embroider/broccoli-side-watch');
+const { Webpack } = require('@embroider/webpack');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+// process.env.BROCCOLI_ENABLED_MEMOIZE = true;
+
+function isProduction() {
+  return EmberApp.env() === 'production';
+}
+
+module.exports = function (defaults) {
+  const app = new EmberApp(defaults, {
+    // Add options here
+    autoImport: {
+      watchDependencies: [
+        // my-addon,
+      ],
+    },
+
+    babel: {
+      plugins: [
+        require.resolve('ember-concurrency/async-arrow-task-transform'),
+      ],
+    },
+
+    'ember-cli-babel': {
+      enableTypeScriptTransform: true,
+    },
+
+    trees: {
+      app: sideWatch('app', {
+        watching: ['../my-addon/src'],
+      }),
+    },
+  });
+
+  const options = {
+    packagerOptions: {
+      cssLoaderOptions: {
+        modules: {
+          localIdentName: isProduction()
+            ? '[sha512:hash:base64:5]'
+            : '[path][name]__[local]',
+          mode: (resourcePath) => {
+            const hostAppLocation = 'my-app';
+
+            return resourcePath.includes(hostAppLocation) ? 'local' : 'global';
+          },
+        },
+        sourceMap: !isProduction(),
+      },
+      publicAssetURL: '/',
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /(node_modules\/\.embroider\/rewritten-app\/)(.*\.css)$/i,
+              use: [
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    sourceMap: !isProduction(),
+                    postcssOptions: {
+                      config: './postcss.config.js',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    splitAtRoutes: ['form', 'product-details', 'products'],
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticEmberSource: true,
+    staticInvokables: true,
+  };
+
+  return require('@embroider/compat').compatBuild(app, Webpack, options);
+};
