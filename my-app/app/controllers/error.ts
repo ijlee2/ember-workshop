8f9b5468f07f0833d8c342ae474aac2ecb268246
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-14 08:55:54 +0200

    Revert "Day 3: Created error and loading routes"

--- my-app/app/controllers/error.ts
+++
@@ -1,5 +0,0 @@
-import Controller from '@ember/controller';
-
-export default class ErrorController extends Controller {
-  declare model: any;
-}
--- my-app/app/controllers/products/error.ts
+++ my-app/app/controllers/products/error.ts
@@ -3,7 +3,5 @@ import Controller from '@ember/controller';
 import styles from './error.css';
 
 export default class ProductsErrorController extends Controller {
-  declare model: any;
-
   styles = styles;
 }
