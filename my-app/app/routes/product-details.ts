a24f1e5058599dcd2d058095e3dd597c9a50ee6e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:57:35 +0200

    day-2: Fetched product

--- my-app/app/routes/product-details.ts
+++ my-app/app/routes/product-details.ts
@@ -1,9 +1,24 @@
+import { action } from '@ember/object';
 import Route from '@ember/routing/route';
+import { type Registry as Services, service } from '@ember/service';
 import type { ModelFrom } from 'my-app/utils/routes';
+import type { Product } from 'my-app/utils/routes/products';
 
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
 export type Model = ModelFrom<ProductDetailsRoute> & {
   id: string;
 };
 
-export default class ProductDetailsRoute extends Route {}
+export default class ProductDetailsRoute extends Route {
+  @service declare api: Services['api'];
+  @service declare router: Services['router'];
+
+  model(params: { id: string }): Promise<Product> {
+    const { id } = params;
+
+    return this.api.get<Product>(`/products/${id}`);
+  }
+
+  @action error(/* error, transition */) {
+    this.router.replaceWith('products');
+  }
+}
