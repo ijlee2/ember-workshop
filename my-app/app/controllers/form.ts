782b8895cd98beef986e827383c6870598e5931b
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:55:35 +0200

    day-1: Covered DDAU

--- my-app/app/controllers/form.ts
+++ my-app/app/controllers/form.ts
@@ -1,3 +1,17 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default class FormController extends Controller {}
+export default class FormController extends Controller {
+  get initialData(): Record<string, unknown> {
+    return {
+      email: undefined,
+      message: 'I 🧡 container queries!',
+      name: undefined,
+      subscribe: true,
+    };
+  }
+
+  @action submitData(data: Record<string, unknown>): void {
+    console.table(data);
+  }
+}
