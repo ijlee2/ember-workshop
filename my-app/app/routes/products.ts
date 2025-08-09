5d44f3de2f657329fb0e631efb63eff46f64f07e
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:56:52 +0200

    day-2: Fetched products

--- my-app/app/routes/products.ts
+++ my-app/app/routes/products.ts
@@ -1,6 +1,14 @@
 import Route from '@ember/routing/route';
+import { type Registry as Services, service } from '@ember/service';
 import type { ModelFrom } from 'my-app/utils/routes';
+import type { Product } from 'my-app/utils/routes/products';
 
 export type Model = ModelFrom<ProductsRoute>;
 
-export default class ProductsRoute extends Route {}
+export default class ProductsRoute extends Route {
+  @service declare api: Services['api'];
+
+  model(): Promise<Product[]> {
+    return this.api.get<Product[]>('/products');
+  }
+}
