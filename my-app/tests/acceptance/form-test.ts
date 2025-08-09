de0bd75e5162ce0a7933bb3bafbdc4d02aa969cc
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:55:02 +0200

    Scaffolded tests

---
+++ my-app/tests/acceptance/form-test.ts
@@ -0,0 +1,134 @@
+import { click, currentURL, fillIn, visit } from '@ember/test-helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+import { getPageTitle } from 'ember-page-title/test-support';
+import { Response } from 'miragejs';
+import {
+  type ApplicationTestContext,
+  setupApplicationTest,
+  setupExperiments,
+} from 'my-app/tests/helpers';
+import { module, skip, test } from 'qunit';
+
+interface TestContext extends ApplicationTestContext {}
+
+module('Acceptance | form', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('Accessibility audit', async function (assert) {
+    await visit('/form');
+    await a11yAudit();
+
+    assert.strictEqual(getPageTitle(), 'Form | Ember Workshop');
+  });
+
+  module('subscribe-to-ember-times, control', function (nestedHooks) {
+    setupExperiments(nestedHooks, {
+      'subscribe-to-ember-times': 'control',
+    });
+
+    skip('We can visit the page', async function (assert) {
+      await visit('/form');
+
+      assert.strictEqual(currentURL(), '/form');
+
+      assert.dom('[data-test-field="Name"]').hasNoValue();
+
+      assert.dom('[data-test-field="Email"]').hasNoValue();
+
+      assert
+        .dom('[data-test-field="Message"]')
+        .hasValue('I 🧡 container queries!');
+
+      assert
+        .dom('[data-test-field="Subscribe to The Ember Times?"]')
+        .doesNotExist();
+
+      assert.dom('[data-test-field="Donation amount ($)"]').hasNoValue('');
+
+      assert.dom('[data-test-error-message]').exists({ count: 2 });
+
+      assert.dom('[data-test-button="Submit"]').isEnabled();
+    });
+
+    skip('We can submit the contact me form', async function (this: TestContext, assert) {
+      this.server.post('/contact-me', (schema, request) => {
+        assert.step('POST /contact-me');
+
+        const json = JSON.parse(request.requestBody);
+
+        assert.deepEqual(json, {
+          data: {
+            attributes: {
+              donation: 10000,
+              email: 'zoey@emberjs.com',
+              message: 'Gude!',
+              name: 'Zoey',
+            },
+            type: 'contact-form',
+          },
+        });
+
+        return new Response(200);
+      });
+
+      await visit('/form');
+
+      await fillIn('[data-test-field="Name"]', 'Zoey');
+      await fillIn('[data-test-field="Email"]', 'zoey@emberjs.com');
+      await fillIn('[data-test-field="Message"]', 'Gude!');
+      await fillIn('[data-test-field="Donation amount ($)"]', '10000');
+
+      assert.dom('[data-test-error-message]').doesNotExist();
+
+      await click('[data-test-button="Submit"]');
+
+      assert.verifySteps(['POST /contact-me']);
+    });
+  });
+
+  module('subscribe-to-ember-times, v1', function (nestedHooks) {
+    setupExperiments(nestedHooks, {
+      'subscribe-to-ember-times': 'v1',
+    });
+
+    skip('We can visit the page', async function (assert) {
+      await visit('/form');
+
+      assert.strictEqual(currentURL(), '/form');
+
+      /*
+        TODO: Write tests
+      */
+    });
+
+    skip('We can submit the contact me form', async function (this: TestContext, assert) {
+      this.server.post('/contact-me', (schema, request) => {
+        assert.step('POST /contact-me');
+
+        const json = JSON.parse(request.requestBody);
+
+        assert.deepEqual(json, {
+          data: {
+            attributes: {
+              email: 'zoey@emberjs.com',
+              message: 'Gude!',
+              name: 'Zoey',
+              subscribe: false,
+            },
+            type: 'contact-form',
+          },
+        });
+
+        return new Response(200);
+      });
+
+      await visit('/form');
+
+      /*
+        TODO: Write tests
+      */
+
+      assert.verifySteps([]);
+    });
+  });
+});
---
+++ my-app/tests/acceptance/product-details-test.ts
@@ -0,0 +1,131 @@
+import { click, currentURL, findAll, visit } from '@ember/test-helpers';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
+import { getPageTitle } from 'ember-page-title/test-support';
+import {
+  type ApplicationTestContext,
+  assertProductDetails,
+  assertProducts,
+  setupApplicationTest,
+  setupExperiments,
+} from 'my-app/tests/helpers';
+import { module, skip } from 'qunit';
+
+interface TestContext extends ApplicationTestContext {}
+
+module('Acceptance | product-details', function (hooks) {
+  setupApplicationTest(hooks);
+
+  hooks.beforeEach(function (this: TestContext) {
+    this.server.create('product', {
+      // @ts-expect-error: Incorrect type
+      categoryId: 'cake',
+      description: 'Made with organic herbs',
+      name: 'Vanilla Ice Cream Cake',
+      price: 40,
+      rating: 4.5,
+      seller: "Amy's",
+      shortDescription: 'Made with organic herbs',
+    });
+
+    this.server.create('product', {
+      // @ts-expect-error: Incorrect type
+      categoryId: 'mug',
+      description: 'A good day starts with a good cup of coffee',
+      name: 'Ember.js Mug',
+      price: 8,
+      rating: 5,
+      seller: 'Ember',
+      shortDescription: 'A good day starts with a good cup of coffee',
+    });
+
+    this.server.create('product', {
+      // @ts-expect-error: Incorrect type
+      categoryId: 'cake',
+      description: 'A chocolate sponge cake with a rich cherry filling',
+      name: 'Black Forest Cake',
+      price: 70,
+      rating: 5,
+      seller: 'The local Konditorei',
+      shortDescription: 'A chocolate sponge cake with a rich cherry filling',
+    });
+  });
+
+  module('nest-product-details, control', function (nestedHooks) {
+    setupExperiments(nestedHooks, {
+      'nest-product-details': 'control',
+    });
+
+    skip('Accessibility audit', async function (assert) {
+      await visit('/product-details/1');
+      await a11yAudit();
+
+      assert.strictEqual(
+        getPageTitle(),
+        'Vanilla Ice Cream Cake | Ember Workshop',
+      );
+    });
+
+    skip('We can visit the page', async function (assert) {
+      await visit('/product-details/1');
+
+      assert.strictEqual(currentURL(), '/product-details/1');
+
+      assertProductDetails(assert, {
+        description: 'Made with organic herbs',
+        name: 'Vanilla Ice Cream Cake',
+        price: '$40',
+        rating: '4.5 out of 5 stars',
+        seller: "Amy's",
+      });
+    });
+
+    skip('We can check the details of another product', async function (assert) {
+      await visit('/product-details/1');
+      await click('[data-test-link="Back"]');
+
+      assert.strictEqual(currentURL(), '/products');
+
+      assertProducts(assert, [
+        'Vanilla Ice Cream Cake',
+        'Ember.js Mug',
+        'Black Forest Cake',
+      ]);
+
+      const products = findAll('[data-test-product-card]');
+
+      await click(products[2]!.querySelector('[data-test-link="Learn More"]')!);
+
+      assert.strictEqual(currentURL(), '/product-details/3');
+
+      assertProductDetails(assert, {
+        description: 'A chocolate sponge cake with a rich cherry filling',
+        name: 'Black Forest Cake',
+        price: '$70',
+        rating: '5 out of 5 stars',
+        seller: 'The local Konditorei',
+      });
+    });
+
+    skip('When we check a product that does not exist, we are redirected to the products route', async function (assert) {
+      await visit('/product-details/not-a-valid-id');
+
+      assert.strictEqual(currentURL(), '/products');
+
+      assertProducts(assert, [
+        'Vanilla Ice Cream Cake',
+        'Ember.js Mug',
+        'Black Forest Cake',
+      ]);
+    });
+  });
+
+  module('nest-product-details, v1', function (nestedHooks) {
+    setupExperiments(nestedHooks, {
+      'nest-product-details': 'v1',
+    });
+
+    /*
+      TODO: Write tests
+    */
+  });
+});
