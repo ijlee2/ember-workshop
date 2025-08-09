2fc14d4dcc3cfb2db5db747c889d8c5f114b9610
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:54:46 +0200

    Created form components

---
+++ my-addon/src/components/ui/form/field.module.css.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+  readonly 'container': string;
+  readonly 'feedback': string;
+  readonly 'field': string;
+  readonly 'is-error': string;
+  readonly 'is-inline': string;
+  readonly 'is-wide': string;
+  readonly 'label': string;
+  readonly 'no-feedback': string;
+};
+
+export default styles;
--- my-addon/src/template-registry.ts
+++ my-addon/src/template-registry.ts
@@ -1,9 +1,33 @@
 import type NavigationMenuComponent from './components/navigation-menu.gts';
+import type UiFormComponent from './components/ui/form.gts';
+import type UiFormCheckboxComponent from './components/ui/form/checkbox.gts';
+import type UiFormFieldComponent from './components/ui/form/field.gts';
+import type UiFormInformationComponent from './components/ui/form/information.gts';
+import type UiFormInputComponent from './components/ui/form/input.gts';
+import type UiFormNumberComponent from './components/ui/form/number.gts';
+import type UiFormSelectComponent from './components/ui/form/select.ts';
+import type UiFormTextareaComponent from './components/ui/form/textarea.gts';
 import type UiPageComponent from './components/ui/page.gts';
 
 export default interface MyAddonRegistry {
   NavigationMenu: typeof NavigationMenuComponent;
+  'Ui::Form': typeof UiFormComponent;
+  'Ui::Form::Checkbox': typeof UiFormCheckboxComponent;
+  'Ui::Form::Field': typeof UiFormFieldComponent;
+  'Ui::Form::Information': typeof UiFormInformationComponent;
+  'Ui::Form::Input': typeof UiFormInputComponent;
+  'Ui::Form::Number': typeof UiFormNumberComponent;
+  'Ui::Form::Select': typeof UiFormSelectComponent;
+  'Ui::Form::Textarea': typeof UiFormTextareaComponent;
   'Ui::Page': typeof UiPageComponent;
   'navigation-menu': typeof NavigationMenuComponent;
+  'ui/form': typeof UiFormComponent;
+  'ui/form/checkbox': typeof UiFormCheckboxComponent;
+  'ui/form/field': typeof UiFormFieldComponent;
+  'ui/form/information': typeof UiFormInformationComponent;
+  'ui/form/input': typeof UiFormInputComponent;
+  'ui/form/number': typeof UiFormNumberComponent;
+  'ui/form/select': typeof UiFormSelectComponent;
+  'ui/form/textarea': typeof UiFormTextareaComponent;
   'ui/page': typeof UiPageComponent;
 }
