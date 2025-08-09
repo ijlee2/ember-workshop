6365c5ab7294c48fec140058095a606a6ca5bbfc
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:57:29 +0200

    day-2: Scaffolded product-details route

---
+++ my-app/app/routes/product-details.ts
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+import type { ModelFrom } from '../utils/routes';
+
+// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+export type Model = ModelFrom<ProductDetailsRoute> & {
+  id: string;
+};
+
+export default class ProductDetailsRoute extends Route {}
