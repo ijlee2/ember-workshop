2092a0cfb7944c8dc1291ee91d2b78856f61f4d3
ijlee2 <ijlee2@users.noreply.github.com>
2025-08-09 12:54:39 +0200

    Created page components

--- my-addon/src/template-registry.ts
+++ my-addon/src/template-registry.ts
@@ -1,1 +1,9 @@
-export default interface MyAddonRegistry {}
+import type NavigationMenuComponent from './components/navigation-menu.gts';
+import type UiPageComponent from './components/ui/page.gts';
+
+export default interface MyAddonRegistry {
+  'navigation-menu': typeof NavigationMenuComponent;
+  NavigationMenu: typeof NavigationMenuComponent;
+  'Ui::Page': typeof UiPageComponent;
+  'ui/page': typeof UiPageComponent;
+}
