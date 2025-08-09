8cc73eadd53784da605cbf29cf3635a21b392a13
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:57:44 +0200

    day-2: Covered query parameters

--- my-app/app/routes/products.ts
+++ my-app/app/routes/products.ts
@@ -5,10 +5,26 @@ import type { Product } from 'my-app/utils/routes/products';
 
 export type Model = ModelFrom<ProductsRoute>;
 
+type Params = {
+  name: string | null;
+};
+
 export default class ProductsRoute extends Route {
+  queryParams = {
+    name: {
+      refreshModel: true,
+    },
+  };
+
   @service declare api: Services['api'];
 
-  model(): Promise<Product[]> {
+  model(params: Params): Promise<Product[]> {
+    const { name } = params;
+
+    if (name) {
+      return this.api.get<Product[]>(`/products?name=${name}`);
+    }
+
     return this.api.get<Product[]>('/products');
   }
 }
