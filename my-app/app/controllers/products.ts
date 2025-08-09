2e644a37f9ca60926c4808176030018d64bf8b13
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:57:39 +0200

    day-2: Filtered products

--- my-app/app/controllers/products.ts
+++ my-app/app/controllers/products.ts
@@ -1,3 +1,26 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
-export default class ProductsController extends Controller {}
+export default class ProductsController extends Controller {
+  @tracked name: string | null = null;
+
+  @action updateQueryParameters({
+    key,
+    value,
+  }: {
+    key: string;
+    value?: string;
+  }): void {
+    if (key !== 'name') {
+      return;
+    }
+
+    if (value === undefined || value === '') {
+      this[key] = null;
+      return;
+    }
+
+    this[key] = value;
+  }
+}
