d4fa76ae88203704827dfaebfc9e261c4f28dfa3
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-08-09 12:54:57 +0200

    Scaffolded routes

---
+++ my-app/app/templates/products.module.css.d.ts
@@ -0,0 +1,10 @@
+declare const styles: {
+  readonly 'filter': string;
+  readonly 'filters': string;
+  readonly 'list': string;
+  readonly 'product-details': string;
+  readonly 'products': string;
+  readonly 'products-with-details': string;
+};
+
+export default styles;
